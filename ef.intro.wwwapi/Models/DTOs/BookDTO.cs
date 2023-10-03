8b5c36234564b5a769bcc9ac6070663635929da7
Ävadakedavr0 <dylandd4@hotmail.be>
2023-10-03 21:01:27 +0200

    added bookdto and try implement automapper

---
+++ ef.intro.wwwapi/Models/DTOs/BookDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ef.intro.wwwapi.Models.DTOs
+{
+    public class BookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+        public string AuthorName { get; set; } //instead of the entire author object
+        public int PublisherId { get; set; }
+        public string PublisherName { get; set; } // instead of the entire publisher object
+    }
+}
---
+++ ef.intro.wwwapi/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ef.intro.wwwapi.Models;
+using ef.intro.wwwapi.Models.DTOs;
+
+namespace ef.intro.wwwapi.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Book, BookDTO>()
+                .ForMember(dest => dest.AuthorName, opt => opt.MapFrom(src => src.Author.FirstName + " " + src.Author.LastName))
+                .ForMember(dest => dest.PublisherName, opt => opt.MapFrom(src => src.Publisher.Name));
+        }
+    }
+}
