8b4f17052a5ab221df918a11c0892a524f4c4291
konas <konas@LAPTOP-1GHMR8NV>
2023-10-04 14:32:45 +0300

    created Publisher model

---
+++ ef.intro.wwwapi/Models/Publisher.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ef.intro.wwwapi.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }        
+    }
+}
