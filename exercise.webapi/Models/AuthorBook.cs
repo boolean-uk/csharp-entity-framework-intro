786b92dac596d570bf8be99ac89f69d9a0839d18
Elias <elias.experis@se.experis.com>
2024-01-30 15:09:29 +0100

    small fix

--- exercise.webapi/Models/AuthorBook.cs
+++ exercise.webapi/Models/AuthorBook.cs
@@ -4,13 +4,9 @@ namespace exercise.webapi.Models
 {
     public class AuthorBook
     {
-    public class BookAuthor
-    {
-
         public int AuthorId { get; set; }
         public Author Author { get; set; } // = null!;
         public int BookId { get; set; }
         public Book Book { get; set; } // = null!;
-    }
     }
 }
