c095213b9adac2150955373fbd8dfb2182c6791d
Benjamin Samuelsen Austestad <ben_7aus@hotmail.com>
2024-01-31 12:42:56 +0100

    Implement author request methods

---
+++ exercise.webapi/Repository/AuthorRepository.cs
@@ -0,0 +1,47 @@
+﻿using exercise.webapi.Data;
+using exercise.webapi.DTOs;
+using exercise.webapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.webapi.Repository
+{
+    public class AuthorRepository : IAuthorRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthorRepository(DataContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<ICollection<GetAuthorDTO>> GetAll()
+        {
+            return await _context.Authors
+                .Include(a => a.Books)
+                .Select(a => new GetAuthorDTO()
+                {
+                    Email = a.Email,
+                    FirstName = a.FirstName,
+                    LastName = a.LastName,
+                    Books = a.Books.Select(b => new GetBookDTO() { Author=b.Author, Id=b.Id, Title=b.Title }).ToList(),
+                })
+                .ToListAsync();
+        }
+
+        public async Task<GetAuthorDTO> GetById(int id)
+        {
+            Author author = await _context.Authors
+                .Include(a => a.Books)
+                .FirstOrDefaultAsync(a => a.Id == id)
+                ?? throw new ArgumentException($"No author with id: {id}");
+            GetAuthorDTO dto = new()
+            {
+                Email = author.Email,
+                FirstName = author.FirstName,
+                LastName = author.LastName,
+                Books = author.Books.Select(b => new GetBookDTO() { Author=b.Author, Id=b.Id, Title=b.Title}).ToList(),
+            };
+            return dto;
+        }
+    }
+}
---
+++ exercise.webapi/Repository/IAuthorRepository.cs
@@ -0,0 +1,11 @@
+﻿using exercise.webapi.DTOs;
+using exercise.webapi.Models;
+
+namespace exercise.webapi.Repository
+{
+    public interface IAuthorRepository
+    {
+        public Task<ICollection<GetAuthorDTO>> GetAll();
+        public Task<GetAuthorDTO> GetById(int id);
+    }
+}
