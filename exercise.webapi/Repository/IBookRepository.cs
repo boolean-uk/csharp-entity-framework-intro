5cb6561d6e6ab5ff6d4c6f3ede6c437d1e581fa1
guro18 <rollgustav95@gmail.com>
2024-01-31 11:54:26 +0100

    completed core

--- exercise.webapi/Repository/IBookRepository.cs
+++ exercise.webapi/Repository/IBookRepository.cs
@@ -5,5 +5,13 @@ namespace exercise.webapi.Repository
     public interface IBookRepository
     {
         public Task<IEnumerable<Book>> GetAllBooks();
+
+        public Task<Book> GetBookById(int bookID);
+
+        public Task<Book> UpdateBookByID(int bookID, string firstname, string lastname);
+
+        public Task<Book> DeleteBookByID(int bookID);
+
+        public Task<Book> CreateNewBook(string title, int authorID);
     }
 }
