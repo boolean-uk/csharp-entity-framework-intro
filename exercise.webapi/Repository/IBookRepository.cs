c7f4801ff5dfd2699dd4f60701077f5c8450a42b
Mateusz Pietrzak <matti3301@gmail.com>
2024-02-02 09:14:29 +0100

    Completed core tasks and 1 extension task

--- exercise.webapi/Repository/IBookRepository.cs
+++ exercise.webapi/Repository/IBookRepository.cs
@@ -1,9 +1,29 @@
 ﻿using exercise.webapi.Models;
+using exercise.webapi.Models.DTOs;
 
 namespace exercise.webapi.Repository
 {
     public interface IBookRepository
     {
+        // ----- Book -----
         public Task<IEnumerable<Book>> GetAllBooks();
+
+        Task<Book> GetBookById(int id);
+
+        Task<Book> UpdateBooksAuthor(int id, int authorId);
+
+        Task<Book> DeleteBook(int id);
+
+        Task<Book> CreateBook(CreateBookDto newBook);
+
+        // ----- Author -------
+        Task<IEnumerable<AuthorDto>> GetAllAuthors();
+        int GetAmountOfAuthors(); 
+        Task<IEnumerable<BookDto>> GetBooksByAuthorId(int authorId);
+
+        // ------- Publisher ---------
+        Task<IEnumerable<PublisherDto>> GetAllPublishers();
+        Task<IEnumerable<BookDto>> GetBooksByPublisherId(int publisherId); 
+
     }
 }
