f729fe699eab29b751fd94a6e8cf03dd4f005034
Rose van der Giessen <Rose van der Giessen>
2024-02-28 14:42:41 +0100

    Core complete

--- exercise.webapi/Endpoints/BookApi.cs
+++ exercise.webapi/Endpoints/BookApi.cs
@@ -1,4 +1,5 @@
-﻿using exercise.webapi.Models;
+﻿using exercise.webapi.DTO;
+using exercise.webapi.Models;
 using exercise.webapi.Repository;
 using static System.Reflection.Metadata.BlobBuilder;
 
@@ -9,12 +10,48 @@ namespace exercise.webapi.Endpoints
         public static void ConfigureBooksApi(this WebApplication app)
         {
             app.MapGet("/books", GetBooks);
+            app.MapGet("/books/{id}", GetBook);
+            app.MapPost("/books", AddBook);
+            app.MapPut("/books/{id}", UpdateBook);
+            app.MapDelete("/books/{id}", DeleteBook);
         }
 
         private static async Task<IResult> GetBooks(IBookRepository bookRepository)
         {
             var books = await bookRepository.GetAllBooks();
-            return TypedResults.Ok(books);
+            return TypedResults.Ok(BookResponseDTO.FromRepository(books));
+        }
+
+        private static async Task<IResult> GetBook(IBookRepository bookRepository, int id)
+        {
+            var book = await bookRepository.GetBook(id);
+            return TypedResults.Ok(new BookResponseDTO(book));
+        }
+
+        private static async Task<IResult> AddBook (IBookRepository bookRepository, CreateBookPayload payload)
+        {
+            var book = await bookRepository.AddBook(payload);
+            return TypedResults.Created($"/{book.Id}", new BookResponseDTO(book));
+        }
+
+        private static async Task<IResult> UpdateBook(IBookRepository bookRepository, int id, UpdateBookPayload payload)
+        {
+            var book = await bookRepository.UpdateBook(id, payload);
+            if (book == null)
+            {
+                return TypedResults.NotFound();
+            }
+            return TypedResults.Ok(new BookResponseDTO(book));
+        }
+
+        private static async Task<IResult> DeleteBook(IBookRepository bookRepository, int id)
+        {
+            var result = await bookRepository.DeleteBook(id);
+            if (result)
+            {
+                return TypedResults.NoContent();
+            }
+            return TypedResults.NotFound();
         }
     }
 }
