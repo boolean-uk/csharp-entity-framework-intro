04296bd52178f7f6af717c7d94392eecf708da16
Muzea001 <muat0072@gmail.com>
2024-09-05 14:42:49 +0200

    Core + 1 Extension

---
+++ exercise.webapi/DTO/AuthorDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace exercise.webapi.DTO
+{
+    public class AuthorDTO
+    {
+
+        public string fullName { get; set; }
+
+        public int Id { get; set; }
+
+        public string email { get; set; }
+    }
+}
