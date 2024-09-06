de22ce089e4c57613ff2094f910a771ba69ce0b1
Anders Hagen Ottersland <anderh0709@gmail.com>
2024-09-06 12:58:48 +0200

    Publisher Endpoint added. This is messy but everything should work

--- exercise.webapi/DtoModels/PublisherDtos/PublisherDto.cs
+++ exercise.webapi/DtoModels/PublisherDtos/PublisherDto.cs
@@ -1,4 +1,5 @@
 ﻿using exercise.webapi.DtoModels.AuthorApiDtos;
+using exercise.webapi.DtoModels.BookApiDtos;
 using exercise.webapi.Models;
 
 namespace exercise.webapi.DtoModels.PublisherDtos
@@ -9,5 +10,14 @@ namespace exercise.webapi.DtoModels.PublisherDtos
 
         //Can duplicate the BookInAuthorDto here
         public ICollection<BookInPublisherDto> Books { get; set; } = new List<BookInPublisherDto>();
+
+        public PublisherDto(Publisher publisher)
+        {
+            Name = publisher.Name;
+            foreach(var b in publisher.Books)
+            {
+                Books.Add(new BookInPublisherDto(b, new BookAuthorDto(b.Author)));
+            }
+        }
     }
 }
---
+++ exercise.webapi/Repository/PublisherRepository.cs
@@ -0,0 +1,26 @@
+﻿using exercise.webapi.Data;
+using exercise.webapi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise.webapi.Repository
+{
+    public class PublisherRepository : IPublisherRepository
+    {
+
+        DataContext _db;
+
+        public PublisherRepository(DataContext db)
+        {
+            _db = db;
+        }
+        public async Task<IEnumerable<Publisher>> GetPublishers()
+        {
+            return await _db.Publishers.Include(p => p.Books).ThenInclude(b => b.Author).ToListAsync();
+        }
+
+        public async Task<Publisher> GetSinglePublisher(int id)
+        {
+            return await _db.Publishers.Include(p => p.Books).ThenInclude(b => b.Author).FirstOrDefaultAsync(p => p.Id == id);
+        }
+    }
+}
