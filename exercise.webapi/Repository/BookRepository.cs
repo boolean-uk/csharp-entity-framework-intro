17d06c63dccc698f5a59fdd106015018f69460f7
ISecka <ISecka@N-NO-01-01-0093>
2024-09-06 11:08:11 +0200

    Finished Core

--- exercise.webapi/Repository/BookRepository.cs
+++ exercise.webapi/Repository/BookRepository.cs
@@ -13,10 +13,39 @@ namespace exercise.webapi.Repository
             _db = db;
         }
 
+        public async Task<Book> Add(Book book)
+        {
+            await _db.Books.AddAsync(book);
+            await _db.SaveChangesAsync();
+            return  await _db.Books.Include(a => a.Author).FirstOrDefaultAsync(x => x.Id == book.Id) ;
+        }
+
+        public async Task<Book> Delete(int id)
+        {
+            var target = await _db.Books.FirstOrDefaultAsync(x => x.Id == id);
+            _db.Remove(id);
+            await _db.SaveChangesAsync();
+            return  target;
+        }
+
+
         public async Task<IEnumerable<Book>> GetAllBooks()
         {
             return await _db.Books.Include(b => b.Author).ToListAsync();
 
+        }
+
+        public async Task<Book>  GetProdById(int id)
+        {
+            return await _db.Books.FirstOrDefaultAsync(p => p.Id == id);
+        }
+
+        public async Task<Book> Update(Book bok1, int id)
+        {
+            var chosenBook = await _db.Books.FirstOrDefaultAsync(p => p.Id == id);
+            chosenBook.Author = bok1.Author;
+            await _db.SaveChangesAsync();
+            return chosenBook;
         }
     }
 }
--- exercise.webapi/Repository/IBookRepository.cs
+++ exercise.webapi/Repository/IBookRepository.cs
@@ -4,6 +4,10 @@ namespace exercise.webapi.Repository
 {
     public interface IBookRepository
     {
-        public Task<IEnumerable<Book>> GetAllBooks();
+        Task<IEnumerable<Book>> GetAllBooks();
+        Task<Book> Add(Book entity);
+        Task<Book> Update(Book bok1, int id);
+        Task<Book> Delete(int id);
+        Task<Book> GetProdById(int id);   
     }
 }
