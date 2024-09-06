fef8bad6e67084985dfc5690f9a654eacd282ac4
Ali Haider Khan <ali_786@live.no>
2024-09-06 23:09:02 +0200

    core done.

---
+++ exercise.webapi/DTO/AuthorResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿using exercise.webapi.Models;
+
+namespace exercise.webapi.DTO
+{
+    public class AuthorResponseDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+        public ICollection<BookDTO> Books { get; set; } = new List<BookDTO>();
+    }
+}
