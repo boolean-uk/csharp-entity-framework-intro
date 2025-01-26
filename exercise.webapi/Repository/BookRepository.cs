9798f8793d07af2ace6ef25f7c0753779bf27b00
ASaug <ASaug@N-NO-01-01-9945>
2025-01-26 23:22:33 +0100

    finished exercise

--- exercise.webapi/Repository/BookRepository.cs
+++ exercise.webapi/Repository/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using exercise.webapi.Data;
+﻿using System.Net;
+using exercise.webapi.Data;
 using exercise.webapi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -17,6 +18,46 @@ namespace exercise.webapi.Repository
         {
             return await _db.Books.Include(b => b.Author).ToListAsync();
 
+        }
+        public async Task<Book> Get(int id)
+        {
+            return await _db.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var result = await _db.Books.FindAsync(id);
+            if (result == null)
+            {
+                return false;
+            }
+            _db.Books.Remove(result);
+            await _db.SaveChangesAsync();
+            return true;
+        }
+
+        public async Task<Book> Add(Book book)
+        {
+            await _db.Books.AddAsync(book);
+            await _db.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Book> Update(int authorid, int bookid)
+        {
+            var author = await _db.Authors.FindAsync(authorid);
+            if (author == null)
+            {
+                return null;
+            }
+            var book = await _db.Books.FindAsync(bookid);
+            if (book == null)
+            {
+                return null;
+            }
+            book.AuthorId = authorid;
+            await _db.SaveChangesAsync();
+            return book;
         }
     }
 }
--- exercise.webapi/Repository/IBookRepository.cs
+++ exercise.webapi/Repository/IBookRepository.cs
@@ -5,5 +5,10 @@ namespace exercise.webapi.Repository
     public interface IBookRepository
     {
         public Task<IEnumerable<Book>> GetAllBooks();
+        Task<Book> Get(int id);
+        Task<bool> Delete(int id);
+        Task<Book> Add(Book book);
+        Task<Book> Update(int authorid, int bookid);
+
     }
 }
