0061080155072805573d6645d7f95bd140f7844c
giarreh <giarreh1@gmail.com>
2025-01-27 04:41:58 +0100

    Finished core

--- exercise.webapi/Models/Author.cs
+++ exercise.webapi/Models/Author.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using exercise.webapi.DTO;
+using System.Text.Json.Serialization;
 
 namespace exercise.webapi.Models
 {
@@ -9,7 +10,6 @@ namespace exercise.webapi.Models
         public string LastName { get; set; }
         public string Email { get; set; }
 
-        [JsonIgnore] // Todo: replace this with DTO approach
-        public ICollection<Book> Books { get; set; } = new List<Book>();
+        public List<BookDTO> Books { get; set; } = new List<BookDTO>();
     }
 }
--- exercise.webapi/Repository/BookRepository.cs
+++ exercise.webapi/Repository/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using exercise.webapi.Data;
+using exercise.webapi.DTO;
 using exercise.webapi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,5 +19,38 @@ namespace exercise.webapi.Repository
             return await _db.Books.Include(b => b.Author).ToListAsync();
 
         }
+        public async Task<Book> GetBook(int id)
+        {
+            return await _db.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }       
+
+        public async Task<IEnumerable<Author>> GetAllAuthors()
+        {
+            return await _db.Authors.ToListAsync();
+        }
+
+        public async Task<Author> GetAuthor(int id)
+        {
+            return await _db.Authors.FirstOrDefaultAsync(a => a.Id == id);
+        }
+        public async Task<Book> UpdateBook(Book book)
+        {
+            _db.Books.Update(book);
+            await _db.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Book> AddBook(Book book)
+        {
+            _db.Books.Add(book);
+            await _db.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<IEnumerable<Book>> GetBooksByAuthorId(int id)
+        {
+            return await _db.Books.Where(b => b.AuthorId == id).ToListAsync();
+        }
+
     }
 }
