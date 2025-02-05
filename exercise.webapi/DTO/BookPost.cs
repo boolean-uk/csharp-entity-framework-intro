426201d51b946768d38e8c5426b34155f4f62679
Tonnes <to.smith.eg@gmail.com>
2025-02-05 14:51:20 +0100

    Core complete

---
+++ exercise.webapi/DTO/BookPost.cs
@@ -0,0 +1,10 @@
+﻿using exercise.webapi.Models;
+
+namespace exercise.webapi.DTO
+{
+    public class BookPost
+    {
+        public string Title { get; set; }
+        public int AuthorId { get; set; }
+    }
+}
--- exercise.webapi/Repository/BookRepository.cs
+++ exercise.webapi/Repository/BookRepository.cs
@@ -1,4 +1,5 @@
 ﻿using exercise.webapi.Data;
+using exercise.webapi.DTO;
 using exercise.webapi.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,7 +17,34 @@ namespace exercise.webapi.Repository
         public async Task<IEnumerable<Book>> GetAllBooks()
         {
             return await _db.Books.Include(b => b.Author).ToListAsync();
+        }
 
+        public async Task<Book> GetBook(int id)
+        {
+            return await _db.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+        }
+
+        public async Task<Book> AddBook(Book book)
+        {
+            _db.Books.Add(book);
+            await _db.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Book> UpdateBook(Book book)
+        {
+            _db.Books.Update(book);
+            await _db.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<bool> DeleteBook(int id)
+        {
+            var target = await _db.Books.Include(b => b.Author).FirstOrDefaultAsync(b => b.Id == id);
+            if (target == null) return false;
+            _db.Books.Remove(target);
+            await _db.SaveChangesAsync();
+            return true;
         }
     }
 }
